cba59c0564608e71dabbb288939f69c7785a2dab
lizzclark <erclark13@outlook.com>
2019-01-29 11:01:35 +0000

    set up testing

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,11 +1,26 @@
 process.env.NODE_ENV = 'test';
+const { expect } = require('chai');
 const app = require('../app');
 const request = require('supertest')(app);
-const { expect } = require('chai');
 const connection = require('../db/connection');
 
 describe('NC news', () => {
-  // after(connection.destroy());
+  beforeEach(() => {
+    // this function gets called before every test
+    return connection.migrate
+      .rollback()
+      .then(() => {
+        return connection.migrate.latest();
+      })
+      .then(() => {
+        return connection.seed.run();
+      });
+  });
+
+  after(() => {
+    connection.destroy();
+  });
+
   describe('/api', () => {
     it('GET / 200 & responds with ...', () => {
       // expect...
@@ -17,3 +32,13 @@ describe('NC news', () => {
     });
   });
 });
+
+// it('GET / 404 client requests non-existent article ID') - so like "/articles/1000000"
+// knex responds with empty array
+// if (!article) return Promise.reject({status: 404, msg: "no such article"})
+// this sends the error object (that we just passed in) down to the catch block
+
+// it('GET / 40 (Bad Request) client requests invalid article ID') - so like "/articles/piglets"
+// knex responds with an error
+
+// 400 - make an errorCodes400 object to reference when you send the 400 - because knex sends back certain codes in its errors
