69bb6a84ab10aa69ba3af30c40bbfd8fdfe752aa
lizzclark <erclark13@outlook.com>
2019-02-01 10:02:04 +0000

    configure production environment

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,9 @@
 const knex = require('knex');
-const dbConfig = require('../knexfile');
+
+const ENV = process.env.NODE_ENV || 'development';
+const dbConfig =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile')[ENV];
 
 module.exports = knex(dbConfig);
