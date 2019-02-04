4743cc9f0b588176e72e97f46ee0f165a1acb26c
lizzclark <erclark13@outlook.com>
2019-02-04 09:30:52 +0000

    error handler displays status as well as message

--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,4 @@
 exports.errorHandler = (err, req, res, next) => {
-  console.log('you graced the error handler with your presence');
   // 500 if this is an unhandled knex error
   if (err.code) {
     res
@@ -8,8 +7,7 @@ exports.errorHandler = (err, req, res, next) => {
   } else {
     // if this is an error defined by NC news, send it back with the relevant status
     const { message, status } = err;
-    console.log(message);
-    res.status(status).send({ message });
+    res.status(status).send({ status, message });
   }
 };
 
