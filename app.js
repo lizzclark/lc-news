2786390f4e9fac2605144bd1355f860d0dd00ed4
lizzclark <erclark13@outlook.com>
2019-02-18 09:31:14 +0000

    enable CORS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const apiRouter = require('./routes/apiRouter');
 const { errorHandler, invalidPathHandler } = require('./errors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
 app.use('/*', invalidPathHandler);
